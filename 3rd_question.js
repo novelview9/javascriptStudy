65a096df1f542667af02a2477e4370b2af687cb0
Luke <novelview9@example.com>
2016-10-29 03:55:21 +0900

    3 quiz

---
+++ 3rd_question.js
@@ -0,0 +1,27 @@
+/*
+Given an integer n, return the largest number that contains exactly n digits.
+
+Example
+
+For n = 2, the output should be
+largestNumber(n) = 99.
+
+Input/Output
+
+[time limit] 4000ms (js)
+[input] integer n
+
+Constraints:
+1 ≤ n ≤ 7.
+
+[output] integer
+
+The largest integer of length n.
+*/
+
+function largestNumber(n) {
+    result = Math.pow(10, n) -1
+    return result
+}
+
+//get to know Math.pow(a,b) a**b
